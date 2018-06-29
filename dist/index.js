0c09b318aa2c8f9e045734467e8125784a47a6de
Kosta Korenkov <kosta@whoot.me>
2018-06-29 18:12:27 +0300

    extract shortId generator

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.Type = exports.Receipt = undefined;var _receipt = require('./receipt');var _receipt2 = _interopRequireDefault(_receipt);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}exports.
+'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.generateShortId = exports.Type = exports.Receipt = undefined;var _receipt = require('./receipt');var _receipt2 = _interopRequireDefault(_receipt);
+var _util = require('./util');function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}exports.
 
-Receipt = _receipt2.default;exports.Type = _receipt.Type;
+Receipt = _receipt2.default;exports.Type = _receipt.Type;exports.generateShortId = _util.generateShortId;
