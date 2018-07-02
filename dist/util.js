3251eb03002b55816bb1dbc0599a74736ae1d454
Kosta Korenkov <kosta@whoot.me>
2018-07-02 18:29:35 +0300

    add uuidToBin and binToUuid

--- dist/util.js
+++ dist/util.js
@@ -1,4 +1,4 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.generateShortId = undefined;var _ethereumjsUtil = require('ethereumjs-util');var _ethereumjsUtil2 = _interopRequireDefault(_ethereumjsUtil);
+'use strict';Object.defineProperty(exports, "__esModule", { value: true });var _ethereumjsUtil = require('ethereumjs-util');var _ethereumjsUtil2 = _interopRequireDefault(_ethereumjsUtil);
 var _base = require('base-58');var _base2 = _interopRequireDefault(_base);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
 
 /**
@@ -7,7 +7,7 @@ var _base = require('base-58');var _base2 = _interopRequireDefault(_base);functi
                                                                                                                                                                          * @param {(string|Buffer)} merchantId - first 8 bytes of merchant id
                                                                                                                                                                          * @returns {string} short id (7 chars)
                                                                                                                                                                          */
-var generateShortId = exports.generateShortId = function generateShortId(investorId, merchantId) {
+var generateShortId = function generateShortId(investorId, merchantId) {
   var payload = Buffer.alloc(24);
 
   if (investorId instanceof Buffer) {
@@ -26,3 +26,44 @@ var generateShortId = exports.generateShortId = function generateShortId(investo
   var hash = _ethereumjsUtil2.default.sha3(payload);
   return _base2.default.encode(hash).substring(0, 7);
 };
+
+/**
+    * Converts uuid string to hex buffer. Does nothing if given key is not a string
+    * @param {string} key - uuid
+    * @returns {Buffer}
+    */
+var uuidToBin = function uuidToBin(key) {
+  return typeof key === 'string' ? new Buffer(key.replace(/-/g, ''), 'hex') : key;
+};
+
+/**
+    * Converts uuid string to hex buffer. Does nothing if given key is not a string
+    * @param {Buffer} key - hex encoded binary key
+    * @returns {string} - uuid string
+    */
+var binToUuid = function binToUuid(key) {
+  if (key instanceof Buffer) {
+    if (key.length === 16) {
+      return [
+      key.slice(0, 4).toString('hex'),
+      key.slice(4, 6).toString('hex'),
+      key.slice(6, 8).toString('hex'),
+      key.slice(8, 10).toString('hex'),
+      key.slice(10, 16).toString('hex')].
+      join('-');
+    }
+    if (key.length === 8) {
+      return [
+      key.slice(0, 4).toString('hex'),
+      key.slice(4, 6).toString('hex'),
+      key.slice(6, 8).toString('hex')].
+      join('-');
+    }
+  }
+  return key;
+};exports.default =
+
+{
+  generateShortId: generateShortId,
+  uuidToBin: uuidToBin,
+  binToUuid: binToUuid };module.exports = exports['default'];
