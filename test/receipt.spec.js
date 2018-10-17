2946f2c972c57543c95121e842da65e47c3dfa31
Kosta Korenkov <kosta@whoot.me>
2018-10-17 21:23:53 +0300

    style: cleaned up tests a bit

--- test/receipt.spec.js
+++ test/receipt.spec.js
@@ -3,13 +3,15 @@ import Receipt, { Type } from '../lib/receipt';
 
 const PRIV = '0x94890218f2b0d04296f30aeafd13655eba4c5bbf1770273276fee52cbe3f2cb4';
 const ADDR = '0x82e8c6cf42c8d1ff9594b17a3f50e94a12cc860f';
+const merchantId = 'af31ef6a-b3a8-4d6a';
+const saleAddr = '0x00112233445566778899aabbccddeeff00112233';
+const investorId = '117c52a9-a459-4a6f-9ed2-20b713a1ecd0';
+const investorAddr = '0x2222222222222222222222222222222222222222';
 
 describe('receipt + signer ', () => {
-  it('should allow to sign and parse whitelist receipt.', (done) => {
+  it('should allow to sign and parse whitelist receipt.', () => {
     const whitelistReceipt = 'AYYP.jgaMXV5sFyqanZgzy8yFN++HcsFYbdUyKOWxmEqCZ3U=.cmQcWcq9eoX8XyLF7T1vUeCJnhljvCngH9OJXmEt7UY=.G93u/wARIjNY+Z/RIiIiIiIiIiIiIiIiIiIiIiIiIiI=';
     const created = 1492754385;
-    const saleAddr = '0x00112233445566778899aabbccddeeff00112233';
-    const investorAddr = '0x2222222222222222222222222222222222222222';
     const whitelist = new Receipt(saleAddr).whitelist(investorAddr, created);
     // test signing
     expect(whitelist.sign(PRIV)).to.eql(whitelistReceipt);
@@ -22,25 +24,20 @@ describe('receipt + signer ', () => {
       signer: ADDR,
       type: Type.WHITELIST,
     });
-    done();
   });
 
-  it('should fail if miliseconds provided for created.', (done) => {
-    const saleAddr = '0x00112233445566778899aabbccddeeff00112233';
-    const investorAddr = '0x2222222222222222222222222222222222222222';
+  it('should fail if miliseconds provided for created.', () => {
     try {
       new Receipt(saleAddr).whitelist(investorAddr, Date.now());
+      throw new Error('should have thrown');
     } catch (e) {
-      done();
+      // success
     }
-    throw new Error('should have thrown');
   });
 
-  it('should allow to sign and parse session receipt.', (done) => {
+  it('should allow to sign and parse session receipt.', () => {
     const sessionReceipt = 'CIYP.BUj1DVxVn/giqS30O+gPdZ8U1Xmwmzi8l6Y1x0Hdark=.bxGXdffLYN6nyyUxbqoEOgYi9mQbd/JA0iTX59UpKrQ=.HBF8UqmkWUpvntIgtxOh7NCvMe9qs6hNalj5n9EAAAA=';
     const created = 1492754385;
-    const investorId = '117c52a9-a459-4a6f-9ed2-20b713a1ecd0';
-    const merchantId = 'af31ef6a-b3a8-4d6a';
     const session = new Receipt().session(investorId, merchantId, created);
     // test signing
     expect(session.sign(PRIV)).to.eql(sessionReceipt);
@@ -54,13 +51,10 @@ describe('receipt + signer ', () => {
     });
     // should allow to get shortId
     expect(session.shortId()).to.eql('9xRuHU7');
-    done();
   });
 
-  it('should allow to sign and parse investment receipt.', (done) => {
+  it('should allow to sign and parse investment receipt.', () => {
     const investReceipt = 'AoYP.n2/6jTR4n11r7lmkYYWZOk3EC5vtWh//IgEJJq+GzDw=.entfhjjnE+sYIFoGaOc/0wgpt86CUn8tRlqbpV3TvN0=.HN3u/wARIjNEVWZ3IiIiIiIiIiIiIiIiIiIiIiIiIiI=.WPmf0QAA1VaXywLSMzMzMzMzMzMzMzMzMzMzMzMzMzM=';
-    const saleAddr = '0x00112233445566778899aabbccddeeff00112233';
-    const investorAddr = '0x2222222222222222222222222222222222222222';
     const affiliateAddr = '0x3333333333333333333333333333333333333333';
     const orderId = '0x44556677';
     const oobpa = 234567890567890;
@@ -81,22 +75,17 @@ describe('receipt + signer ', () => {
       signer: ADDR,
       type: Type.INVEST,
     });
-    done();
   });
 
-  it('should allow to get short Id.', (done) => {
-    const investorId = '117c52a9-a459-4a6f-9ed2-20b713a1ecd0';
-    const merchantId = 'af31ef6a-b3a8-4d6a';
+  it('should allow to get short Id.', () => {
     const session = new Receipt().session(investorId, merchantId);
     // should allow to get shortId
     expect(session.shortId()).to.eql('9xRuHU7');
-    done();
   });
 
-  it('should allow to sign and parse message receipt.', (done) => {
+  it('should allow to sign and parse message receipt.', () => {
     const messageReceipt = 'KYYP.r0ciB80ze2ZDYqQMPAuB1ML5E90Sk67COKMr27Ycs8E=.cDaKjZsdWxiwWbBTzNfho0ePAXDUps3IpsNh8Mk48ps=.HAABW48YSGivMe9qs6hNagAAAAAAAAAAAAAAAAAAAAc=.bWVzc2FnZQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=';
     const created = 1492754385000;
-    const merchantId = 'af31ef6a-b3a8-4d6a';
     const msg = new Receipt(merchantId).message('message', created);
     // test signing
     expect(msg.sign(PRIV)).to.eql(messageReceipt);
@@ -108,13 +97,11 @@ describe('receipt + signer ', () => {
       signer: ADDR,
       type: Type.MESSAGE,
     });
-    done();
   });
 
-  it('should allow to sign and parse message receipt with BMP chars.', (done) => {
+  it('should allow to sign and parse message receipt with BMP chars.', () => {
     const messageReceipt = 'KYYP.kPRzkuKXLAAvG64QzvgkiVQbwKzYttZT2Q8oB8YH0l4=.YRdb0fEhaPabTX4DUMX4UMXW5sUNqMl2IEeh6nL/Jns=.HAABW48YSGivMe9qs6hNagAAAAAAAAAAAAAAAAAAABc=.SGVsbG/nq5wgw7Yg4oKsIM6pIPCdhJ4AAAAAAAAAAAA=';
     const created = 1492754385000;
-    const merchantId = 'af31ef6a-b3a8-4d6a';
     const msg = new Receipt(merchantId).message('Hello竜 ö € Ω 𝄞', created);
     // test signing
     expect(msg.sign(PRIV)).to.eql(messageReceipt);
@@ -126,13 +113,11 @@ describe('receipt + signer ', () => {
       signer: ADDR,
       type: Type.MESSAGE,
     });
-    done();
   });
 
-  it('should allow to sign and parse message receipt longer than 32 bytes.', (done) => {
+  it('should allow to sign and parse message receipt longer than 32 bytes.', () => {
     const messageReceipt = 'KYYP.vaQdLdl23PUN2n0dEK3V8kSWYT2sOskqc2Knic6jlLQ=.GiwTtRNjf5gpAck1WEtOLllNwuf/K6+M6IJ5jJgaPdU=.GwABW48YSGivMe9qs6hNagAAAAAAAAAAAAAAAAAAAEE=.bWVzc2FnZSB0aGF0IGhhcyBtb3JlIGNoYXJhY3RlcnM=.IHRoYW4gMzIgYnl0ZXMsIDY1IHRvIGJlIHByZWNpc2U=.LgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=';
     const created = 1492754385000;
-    const merchantId = 'af31ef6a-b3a8-4d6a';
     const text = 'message that has more characters than 32 bytes, 65 to be precise.';
     const msg = new Receipt(merchantId).message(text, created);
     // test signing
@@ -145,6 +130,5 @@ describe('receipt + signer ', () => {
       signer: ADDR,
       type: Type.MESSAGE,
     });
-    done();
   });
 });
